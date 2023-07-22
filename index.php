98cb592bb7bb95fc9c64c38fd96313a2b0c29afc
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-07-22 20:45:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <p>Today's visitors: <?php echo $decodedCountJson[$today] ?></p>
         <p>Yesterday's visitors: <?php echo $decodedCountJson[$yesterday] ?></p>
         <p>With <?php echo $max_visitors ?> visitors, <?php echo $max_day ?> was the day with most traffic</p>
-        <p>Total Visitors: <?php echo $decodedCountJson["total"] ?></p>
+        <p>Total visits: <?php echo $decodedCountJson["total"] ?></p>
     </div>
 </body>
 </html>
